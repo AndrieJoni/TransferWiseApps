5e71295545bd8f4a61ea8017474fbe728baa0d39
Andriej10 <andriejoni10@gmail.com>
2020-10-22 01:08:49 +0700

    first commit

---
+++ app/src/androidTest/java/parkee/parkee/transferwiseapps/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package parkee.parkee.transferwiseapps
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("parkee.parkee.transferwiseapps", appContext.packageName)
+    }
+}
